3abaad888d2a0c9b21936f32753de451c0ed2388
kasapuavish001 <kasapuavish001@gmail.com>
2023-08-27 22:24:56 +0530

    feat:added update

--- app/components/Display/index.jsx
+++ app/components/Display/index.jsx
@@ -3,26 +3,54 @@ import Card from '../card'
 import styles from './display.module.scss'
 
 const Display = () => {
-  return (
-    <div className={styles.em_display}>
-         <div className={styles.bar}>
-          <p className={styles.title}><span>32</span> Employee</p>
-          <div className={styles.btns}>
-                  <button className={styles.btn1}><Filter className={styles.icon }/> <span>Filter</span></button>
-                  <button  className={styles.btn2}>+ Add Candidate</button>
-          </div>  
-    </div>
-      <div className={styles.cardwrap}>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      </div>
-      
-    </div>
-  )
+    return (
+        <div className={styles.em_display}>
+            <div className={styles.title}>
+                <h1>Add New Employee</h1>
+            </div>
+            <form className={styles.add}>
+                <div className={styles.wrap}>
+                    <div className={styles.top}>
+                        <div className={styles.box}>
+                            <label>First Name</label>
+                            <input type="text" />
+                        </div>
+                        <div className={styles.box}>
+                            <label>Last Name</label>
+                            <input type="text" />
+                        </div>
+                        <div className={styles.box}>
+                            <label>Job Position</label>
+                            <input type="text" />
+                        </div>
+                    </div>
+                    <div className={styles.bottom}>
+                        <div className={styles.box}>
+                            <label>Email Address</label>
+                            <input type="email" />
+                        </div>
+                        <div className={styles.box}>
+                            <label>Phone</label>
+                            <input type="number" />
+                        </div>
+                        <div className={styles.box}>
+                            <label>Date of Joining</label>
+                            <input type="date" />
+                        </div>
+                        <div className={styles.box}>
+                            <label>Salary</label>
+                            <input type="number" />
+                        </div>
+                    </div>
+                    <div className={styles.buttons}>
+                            <button className={`${styles.btn} ${styles.btn1}`}>Save</button>
+                            <button className={`${styles.btn} ${styles.btn2}`}>Save & Add Another</button>
+                            <button className={`${styles.btn} ${styles.btn3}`}>Cancel</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default Display
--- app/components/navbar/index.jsx
+++ app/components/navbar/index.jsx
@@ -5,96 +5,96 @@ import Box from '../../../public/icons/tool-box.svg'
 import Calender from '../../../public/icons/calendar.svg'
 import Display from '../Display'
 
-const data = [ 
+const data = [
     {
-        id:1,
-        name : "Dashboard",
-        icon : <App className={styles.icon}/> 
+        id: 1,
+        name: "Dashboard",
+        icon: <App className={styles.icon} />
     },
     {
-        id:2,
-        name : "Inbox",
-        icon : <Box className={styles.icon}/> 
+        id: 2,
+        name: "Inbox",
+        icon: <Box className={styles.icon} />
     },
     {
-        id:3,
-        name : "Calender &Todo's",
-        icon : <Calender className={styles.icon}/> 
+        id: 3,
+        name: "Calender &Todo's",
+        icon: <Calender className={styles.icon} />
     },
 ]
 
 const data1 = [
     {
-        id:4,
-        name : "Jobs",
-        icon : <App className={styles.icon}/> 
+        id: 4,
+        name: "Jobs",
+        icon: <App className={styles.icon} />
     },
     {
-        id:5,
-        name : "Candidates",
-        icon : <Box className={styles.icon}/> 
+        id: 5,
+        name: "Candidates",
+        icon: <Box className={styles.icon} />
     },
     {
-        id:6,
-        name : "My Referrals",
-        icon : <App className={styles.icon}/> 
+        id: 6,
+        name: "My Referrals",
+        icon: <App className={styles.icon} />
     },
 ]
 
 const data2 = [
     {
-        id:7,
-        name : "Career site",
-        icon : <Calender className={styles.icon}/> 
+        id: 7,
+        name: "Career site",
+        icon: <Calender className={styles.icon} />
     },
     {
-        id:8,
-        name : "Setting",
-        icon : <Box className={styles.icon}/> 
+        id: 8,
+        name: "Setting",
+        icon: <Box className={styles.icon} />
     },
 ]
 const Navbar = () => {
-  return (
-    <section>
-        <div className="contain">
-            <div className={styles.wrap}>
-                <ul className={styles.list1}>
-                    {data.map((item)=>{
-                        return (
-                    <li key={item} className={styles.li}>
-                        {item.icon}
-                        <p>{item.name}</p>
-                    </li>
-                        )
-                    })}
-                </ul>
-                <ul className={styles.list1}>
-                    <h3>RECRUITMENT</h3>
-                    {data1.map((item)=>{
-                        return (
-                    <li key={item} className={styles.li}>
-                        {item.icon}
-                        <p>{item.name}</p>
-                    </li>
-                        )
-                    })}
-                </ul>
-                <ul className={styles.list1}>
-                    <h3>ORGANIZATION</h3>
-                    {data2.map((item)=>{
-                        return (
-                    <li key={item} className={styles.li}>
-                        {item.icon}
-                        <p>{item.name}</p>
-                    </li>
-                        )
-                    })}
-                </ul>
+    return (
+        <section>
+            <div className="contain">
+                <div className={styles.wrap}>
+                    <ul className={styles.list1}>
+                        {data.map((item) => {
+                            return (
+                                <li key={item} className={styles.li}>
+                                    {item.icon}
+                                    <p>{item.name}</p>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                    <ul className={styles.list1}>
+                        <h3>RECRUITMENT</h3>
+                        {data1.map((item) => {
+                            return (
+                                <li key={item} className={styles.li}>
+                                    {item.icon}
+                                    <p>{item.name}</p>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                    <ul className={styles.list1}>
+                        <h3>ORGANIZATION</h3>
+                        {data2.map((item) => {
+                            return (
+                                <li key={item} className={styles.li}>
+                                    {item.icon}
+                                    <p>{item.name}</p>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>
+                <Display />
             </div>
-            <Display/>
-        </div>
-    </section>
-  )
+        </section>
+    )
 }
 
 export default Navbar
