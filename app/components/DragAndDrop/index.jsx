16fbb1cac80177b66305271362eb351c1bd16e6a
kasapuavish001 <kasapuavish001@gmail.com>
2023-08-27 22:33:42 +0530

    feat : fixed update

--- app/components/DragAndDrop/index.jsx
+++ app/components/DragAndDrop/index.jsx
@@ -1,138 +1,64 @@
-import Image from "next/image";
-import { useState, useEffect } from "react"
-import Dropzone from 'react-dropzone'
+import React, { useState } from "react";
+import Dropzone from 'react-dropzone';
 import { Controller } from "react-hook-form";
-import FormFields from '../../Models/FormFields.json'
+import FormFields from '../../Models/FormFields.json';
 const DragAndDrop = ({
     name,
-    parent,
     control,
     type,
-    styles,
-    wrapper = false,
-    label,
-    inputWrapper = true,
-    Icon = false,
-    Img,
-    setImg,
-    airstrike,
-    isDragAccept,
     setValue,
-    watch,
-    clearErrors
+    clearErrors,
 }) => {
-    const Field = parent ? (FormFields[parent])[name] : FormFields[name]
-    const InputWrapper = inputWrapper ? 'div' : React.Fragment
+    const Field = FormFields[name];
+    const handleImageDrop = (acceptedFiles) => {
+        const file = acceptedFiles[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = () => {
+                const base64String = reader.result.split(',')[1]; // Extract base64 part
+                setValue(name, base64String);
+                clearErrors(name);
+            };
+            reader.readAsDataURL(file);
+        }
+    };
     return (
-        <>
-            <Controller
-                isDragAccept={isDragAccept}
-                control={control}
-                name={name}
-                render={({ field, fieldState: { error } }) => (
-                    <>
-                        {
-                            label &&
-                            <label
-                                className={styles.FormLabel}
-                                htmlFor={name}
-                            >{Field.label} {airstrike ? <span style={{ color: "red" }}>*</span> : ""}
-                            </label>
-                        }
-                        <Dropzone
-                            onDrop={(acceptedFiles,) => {
-                                setValue(name, {
-                                    type: 'local',
-                                    dataType: name,
-                                    data: acceptedFiles[0],
-                                    url: URL.createObjectURL(acceptedFiles[0])
-                                })
-                                clearErrors(name)
-                            }}
-
-                            accept={
-                                type === 'file' ?
-                                    { 'application/pdf': ['.pdf'] } :
-                                    { 'image/png': ['.png', '.jpg', '.jpeg', '.webp'] }
-                            }
-                        >
-                            {({ getRootProps, getInputProps }) => (
-                                <section className={styles.dragbox}>
-                                    <div {...getRootProps()}>
-                                        <input {...getInputProps()} />
-                                        <>
-                                            {
-                                                watch(name)?.type ?
-                                                    <>
-                                                        <div className={styles.imageblock}>
-                                                            <Image
-
-                                                                fill
-                                                                sizes="100"
-                                                                style={{
-                                                                    objectFit: 'cover',
-                                                                    objectPosition: 'center',
-                                                                }}
-                                                                alt="png"
-                                                                src=
-                                                                {
-                                                                    watch(name) &&
-                                                                    watch(name).image ||
-                                                                    watch(name)?.url
-                                                                }
-                                                                className={styles.image_select}
-                                                            />
-                                                        </div>
-                                                        <button type="button" className={styles.updatebtn}>Update</button>
-                                                    </>
-                                                    :
-                                                    <>
-                                                        <>
-                                                            <div className={styles.imageblock}>
-                                                                <Image
-                                                                    fill
-                                                                    sizes="100"
-                                                                    style={{
-                                                                        objectFit: 'cover',
-                                                                        objectPosition: 'center',
-                                                                    }}
-                                                                    alt="png"
-                                                                    src=
-                                                                    {
-                                                                        Img
-                                                                    }
-                                                                    className={styles.image_select}
-                                                                />
-                                                            </div>
-                                                            <button type="button" className={styles.updatebtn}>Update</button>
-                                                        </>
-                                                        {/* {Field.url != "" ? <Image
-                                                            fill
-                                                            sizes="100"
-                                                            style={{
-                                                                objectFit: 'cover',
-                                                                objectPosition: 'center',
-                                                            }}
-                                                            alt="png"
-                                                            src=
-                                                            {
-                                                                Field.url
-                                                            }
-                                                            className={styles.image_select}
-                                                        /> : <> <p>Drag N Drop A Img</p> {error && <p className={styles.error_msg}>{error.message}</p>}</>} */}
-                                                    </>
-
-                                            }
-                                        </>
+        <Controller
+            control={control}
+            name={name}
+            render={({ field }) => (
+                <Dropzone
+                    onDrop={handleImageDrop}
+                    accept={
+                        type === 'file'
+                            ? { 'application/pdf': ['.pdf'] }
+                            : { 'image/png': ['.png', '.jpg', '.jpeg', '.webp'] }
+                    }
+                >
+                    {({ getRootProps, getInputProps }) => (
+                        <section>
+                            <div {...getRootProps()}>
+                                <input {...getInputProps()} />
+                                {field.value && field.value.url ? (
+                                    <div>
+                                        <img
+                                            src={field.value.url}
+                                            alt="Uploaded"
+                                            style={{ width: '100px', height: '100px', objectFit: 'cover' }}
+                                        />
+                                        <button type="button">Update</button>
                                     </div>
-                                </section>
-                            )}
-                        </Dropzone>
-                    </>
-                )}
-            />
-        </>
-    )
-}
-
-export default DragAndDrop
+                                ) : (
+                                    <div>
+                                        <p>Drag and drop an image here</p>
+                                    </div>
+                                )}
+                            </div>
+                        </section>
+                    )}
+                </Dropzone>
+            )}
+        />
+    );
+};
+export default DragAndDrop;
--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Link from "next/link";
+import MyComponent from "./components/test";
 
 
 export default function Home() {
   return (
     <h1>Home
         <Link href='/pages/login'>Login</Link>
+        <MyComponent/>
     </h1>
   )
 }
